8313063edb30661c00d64dc7bee61babbf33ad21
SchafferNoa <noasch@mta.ac.il>
2021-12-31 11:50:06 +0200

    בדיקה

--- JS/associationFormJS.js
+++ JS/associationFormJS.js
@@ -1,50 +1,58 @@
-alert("hello");
-
+// form validation - add messages to required fields that are empty
 function validateForm() {
-    var checkEmail = document.getElementById("email");
-    var stopFirst = checkEmail.indexOf("@");
-    var stopSecond = checkEmail.lastIndexOf(".");
-    if (stopSecond + 2 >= checkEmail.length || stopFirst < 1 || stopSecond < stopFirst + 2) {
-        window.alert("The email address is not valid");
-        checkEmail.focus();
+    // first name validation
+    let x = document.forms["ourForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
         return false;
     }
-
-    var first_name = document.formS.first_name;
-    var last_name = document.formS.last_name;
-    var tel = document.formS.tel;
-
-    var letters = /^[A-Za-z]+$/;
-    if (first_name.value.match(letters)) {
-        return true;
-    }
-    else if (first_name.value.match(letters) == False) {
-        window.alert("The first name need to be only letters");
-    }
-    if (last_name.value.match(letters)) {
-        return true;
-    }
-    else if (last_name.value.match(letters) == False) {
-        window.alert("The last name need to be only letters");
-    }
-
 }
 
+// function validateForm() {
+//     var checkEmail = document.getElementById("email");
+//     var stopFirst = checkEmail.indexOf("@");
+//     var stopSecond = checkEmail.lastIndexOf(".");
+//     if (stopSecond + 2 >= checkEmail.length || stopFirst < 1 || stopSecond < stopFirst + 2) {
+//         window.alert("The email address is not valid");
+//         checkEmail.focus();
+//         return false;
+//     }
 
-document.getElementById("flexCheckChecked").onclick = function () {
-    var otherCheckbox = document.querySelector('input[value="other"]');
-    var otherText = document.querySelector('input[id="otherValue"]');
-    otherText.style.visibility = 'hidden';
+//     var first_name = document.formS.first_name;
+//     var last_name = document.formS.last_name;
+//     var tel = document.formS.tel;
 
-    otherCheckbox.addEventListener('change', () => {
-        if (otherCheckbox.checked) {
-            otherText.style.visibility = 'visible';
-            otherText.value = '';
-        } else {
-            otherText.style.visibility = 'hidden';
-        }
-    });
-}
+//     var letters = /^[A-Za-z]+$/;
+//     if (first_name.value.match(letters)) {
+//         return true;
+//     }
+//     else if (first_name.value.match(letters) == False) {
+//         window.alert("The first name need to be only letters");
+//     }
+//     if (last_name.value.match(letters)) {
+//         return true;
+//     }
+//     else if (last_name.value.match(letters) == False) {
+//         window.alert("The last name need to be only letters");
+//     }
+
+// }
+
+
+// document.getElementById("flexCheckChecked").onclick = function () {
+//     var otherCheckbox = document.querySelector('input[value="other"]');
+//     var otherText = document.querySelector('input[id="otherValue"]');
+//     otherText.style.visibility = 'hidden';
+
+//     otherCheckbox.addEventListener('change', () => {
+//         if (otherCheckbox.checked) {
+//             otherText.style.visibility = 'visible';
+//             otherText.value = '';
+//         } else {
+//             otherText.style.visibility = 'hidden';
+//         }
+//     });
+// }
 
 
 
