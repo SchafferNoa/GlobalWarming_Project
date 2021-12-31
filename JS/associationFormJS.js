4ead5f9f7922b8cc6012777a10fc67689baef84a
SchafferNoa <noasch@mta.ac.il>
2021-12-31 13:02:55 +0200

    עבודה על הטופס

--- JS/associationFormJS.js
+++ JS/associationFormJS.js
@@ -1,42 +1,42 @@
-// form validation - add messages to required fields that are empty
-function validateForm() {
-    // first name validation
-    let x = document.forms["ourForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-
+// // form validation - add messages to required fields that are empty
 // function validateForm() {
-//     var checkEmail = document.getElementById("email");
-//     var stopFirst = checkEmail.indexOf("@");
-//     var stopSecond = checkEmail.lastIndexOf(".");
-//     if (stopSecond + 2 >= checkEmail.length || stopFirst < 1 || stopSecond < stopFirst + 2) {
-//         window.alert("The email address is not valid");
-//         checkEmail.focus();
+//     // last name validation
+//     let lastName = document.forms["ourForm"]["lastName"].value;
+//     if (firstName == "" || lastName == "" || Phone == "" || email == "") {
+//         alert("All fields marked with '*' must be filled out");
 //         return false;
 //     }
-
-//     var first_name = document.formS.first_name;
-//     var last_name = document.formS.last_name;
-//     var tel = document.formS.tel;
-
-//     var letters = /^[A-Za-z]+$/;
-//     if (first_name.value.match(letters)) {
-//         return true;
-//     }
-//     else if (first_name.value.match(letters) == False) {
-//         window.alert("The first name need to be only letters");
-//     }
-//     if (last_name.value.match(letters)) {
-//         return true;
-//     }
-//     else if (last_name.value.match(letters) == False) {
-//         window.alert("The last name need to be only letters");
-//     }
-
 // }
+
+function validateForm() {
+    var checkEmail = document.getElementById("email");
+    var stopFirst = checkEmail.indexOf("@");
+    var stopSecond = checkEmail.lastIndexOf(".");
+    if (stopSecond + 2 >= checkEmail.length || stopFirst < 1 || stopSecond < stopFirst + 2) {
+        window.alert("The email address is not valid");
+        checkEmail.focus();
+        return false;
+    }
+
+    var first_name = document.formS.first_name;
+    var last_name = document.formS.last_name;
+    var tel = document.formS.tel;
+
+    var letters = /^[A-Za-z]+$/;
+    if (first_name.value.match(letters)) {
+        return true;
+    }
+    else if (first_name.value.match(letters) == False) {
+        window.alert("The first name need to be only letters");
+    }
+    if (last_name.value.match(letters)) {
+        return true;
+    }
+    else if (last_name.value.match(letters) == False) {
+        window.alert("The last name need to be only letters");
+    }
+
+}
 
 
 // document.getElementById("flexCheckChecked").onclick = function () {
